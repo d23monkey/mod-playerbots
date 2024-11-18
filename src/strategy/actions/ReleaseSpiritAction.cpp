22e1c9f653a073a90d243e4480fe7cf64b70e104
D23Monkey <dyw23@126.com>
2024-11-18 16:52:22 +0800

    汉化

--- src/strategy/actions/ReleaseSpiritAction.cpp
+++ src/strategy/actions/ReleaseSpiritAction.cpp
@@ -24,15 +24,15 @@ bool ReleaseSpiritAction::Execute(Event event)
 
     if (bot->GetCorpse() && bot->HasFlag(PLAYER_FLAGS, PLAYER_FLAGS_GHOST))
     {
-        botAI->TellMasterNoFacing("I am already a spirit");
+        botAI->TellMasterNoFacing("我已经是一个灵魂了");
         return false;
     }
 
     WorldPacket& p = event.getPacket();
     if (!p.empty() && p.GetOpcode() == CMSG_REPOP_REQUEST)
-        botAI->TellMasterNoFacing("Releasing...");
+        botAI->TellMasterNoFacing("正在释放...");
     else
-        botAI->TellMasterNoFacing("Meet me at the graveyard");
+        botAI->TellMasterNoFacing("在墓地见我");
 
     // Death Count to prevent skeleton piles
     Player* master = GetMaster();
